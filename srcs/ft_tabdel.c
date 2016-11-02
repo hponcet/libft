dbf57666657a81dc049bad0219e7e01bc19f73d2
Hugues Poncet <hugues.poncet@icloud.com>
2016-11-02 20:12:30 +0100

    More protect on NULL entry

--- srcs/ft_tabdel.c
+++ srcs/ft_tabdel.c
@@ -6,7 +6,7 @@
 /*   By: hponcet <hponcet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/15 16:18:52 by hponcet           #+#    #+#             */
-/*   Updated: 2016/09/15 17:13:29 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/11/02 20:11:06 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void		ft_tabdel(char **table)
 	int		i;
 	
 	i = 0;
+	if (!table)
+		return ;
 	while (table[i])
 	{
 		ft_strdel(&(table[i]));
